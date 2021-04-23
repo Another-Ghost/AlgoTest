6dfb55eaf06e781a20b16e3e0e89684e3000c245
AnotherGhost <lb284542@gmail.com>
2021-04-23 21:02:52 +0100

    add dijkstra

--- Graph/GraphTemplate.cpp
+++ Graph/GraphTemplate.cpp
@@ -4,35 +4,33 @@
 
 using namespace std;
 
+//时间复杂度O(N^2)（N为顶点数），适用于稠密图
+//注意一般题目顶点下标以1为起点，注意变更数组长度和遍历起始值
+//LeetCode 743
 namespace Dijkstra
 {
 	int dijkstra(vector<vector<int>>& g, int start, int end)
 	{
 		int n = g.size();
-		vector<int> dist(n, 0x3f3f3f3f); //保存所有点到0号点的最短距离
-		vector<bool> st(n);
-		dist[start] = 0;	
+		vector<int> dist(n, INT_MAX / 2); //g中的标记不存在的边的最大值不能大于 INT_MAX / 2
+		vector<bool> st(n); //为true说明该点已在当前已确定最短路径的点的集合
+		dist[start] = 0; //哪个点为起点就先把哪个点置为0	
 		for (int i = 0; i < n; ++i)
 		{
-			int t = -1;
+			int t = -1; //点下标
 			for (int j = 0; j < n; ++j)
 			{
-				if (!st[j] && (t == -1 || dist[t] > dist[j]))
+				if (!st[j] && (t == -1 || dist[t] > dist[j]))	//找到当前距离start最近的点
 					t = j;
-				st[t] = true;
 			}
-			
+			st[t] = true; //将该点加入 当前已确定最短路径的点 的集合
+
 			for (int j = 0; j < n; ++j)
-				dist[j] = min(dist[j], dist[t] + g[t][j]);
+				dist[j] = min(dist[j], dist[t] + g[t][j]);	//用该点t更新其他点到start的距离
 		}
 
-		if (dist[end] == 0x3f3f3f3f)
-			return -1;
-		else
-			return dist[end];
+		return (dist[end] != INT_MAX / 2) ? dist[end] : -1;
 	}
-
-	
 }
 
 
--- Tree/TreeTemplate.cpp
+++ Tree/TreeTemplate.cpp
@@ -7,61 +7,35 @@ using namespace std;
 const int N = 10010;
 
 
-//int son[N][26], idx;
-//int cnt[N]; //有多少个以该节点结尾的单词（同一单词）
-////vector<vector<int>> cnt(N);
-//
-//void insert(const string& str)
-//{
-//	int p = 0;
-//	for (int i = 0; str[i]; ++i)
-//	{
-//		int u = str[i] - 'a';
-//		if (!son[p][u])
-//			son[p][u] = ++idx;
-//		p = son[p][u];	//p指向下一个字母
-//	}
-//	++cnt[p];
-//}
-//
-//int find(const string& str) 
-//{
-//	int p = 0;
-//	for (int i = 0; str[i]; ++i)
-//	{
-//		int u = str[i] - 'a';
-//		if (!son[p][u])
-//			return 0;
-//		p = son[p][u];
-//	}
-//	return cnt[p];
-//}
+int son[N][26], idx;
+int cnt[N]; //有多少个以该节点结尾的单词（同一单词）
+//vector<vector<int>> cnt(N);
 
-int son[N][26], cnt[N], idx;
-
-void Insert(const string& str)
+void insert(const string& str)
 {
 	int p = 0;
 	for (int i = 0; str[i]; ++i)
 	{
-		int ch = str[i] - 'a';
-		if (!son[p][ch])
-			son[p][ch] = idx++;
-		p = son[p][ch];
+		int u = str[i] - 'a';
+		if (!son[p][u])
+			son[p][u] = ++idx;
+		p = son[p][u];	//p指向下一个字母
 	}
 	++cnt[p];
 }
 
-int Find(const string& str)
+int find(const string& str) 
 {
 	int p = 0;
 	for (int i = 0; str[i]; ++i)
 	{
-		int ch = str[i] - 'a';
-		if (!son[p][ch])
-			son[p][ch] = idx++;
-		p = son[p][ch];
+		int u = str[i] - 'a';
+		if (!son[p][u])
+			return 0;
+		p = son[p][u];
 	}
 	return cnt[p];
 }
+
+
 
